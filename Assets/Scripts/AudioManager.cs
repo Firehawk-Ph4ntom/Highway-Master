3fb5b8697b8a20471e61a725ba2ee1ff1bc60415
Firehawk-Ph4ntom <87373283+Firehawk-Ph4ntom@users.noreply.github.com>
2026-05-27 21:32:06 +0300

    Added Simple AudioManager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    public AudioMixer audioMixer;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void SetMasterVolume(float volume)
+    {
+        audioMixer.SetFloat("MasterVolume", Mathf.Log10(volume) * 20);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", Mathf.Log10(volume) * 20);
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        audioMixer.SetFloat("SFXVolume", Mathf.Log10(volume) * 20);
+    }
+
+    public void SetAmbientVolume(float volume)
+    {
+        audioMixer.SetFloat("AmbientVolume", Mathf.Log10(volume) * 20);
+    }
+}
