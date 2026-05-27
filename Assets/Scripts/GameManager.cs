715b2c0d98dffd7defc7b15190068685c75f38d9
Firehawk-Ph4ntom <87373283+Firehawk-Ph4ntom@users.noreply.github.com>
2026-05-27 15:09:46 +0300

    Added Player Car Sprite and linked Score to GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,7 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    public TextMeshProUGUI scoreText;
     private int score = 0;
     public bool gameOver = false;
 
@@ -11,10 +12,14 @@ public class GameManager : MonoBehaviour
     {
         score = 0;
         gameOver = false;
+        scoreText.text = "Score: " + score;
     }
 
-    void Update()
+    public void AddScore()
     {
-        
+        if (!gameOver) {
+            score++;
+            scoreText.text = "Score: " + score;
+        }
     }
 }
