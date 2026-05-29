73585f77ba6c467b6f7edae6df923ee1ba616fa4
Firehawk-Ph4ntom <87373283+Firehawk-Ph4ntom@users.noreply.github.com>
2026-05-29 18:14:31 +0300

    Added clouds and macro texture

---
+++ Assets/Scripts/Cloud.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Cloud : MonoBehaviour
+{
+    public float moveSpeed = 1.5f;
+    public Vector2 moveDirection = new Vector2(-0.3f, -1.0f);
+    public float destroyY;
+
+    private void Update()
+    {
+        // Clouds move downward in the scene, and are destroyed on passing the destroyY threshold
+        if (!FindFirstObjectByType<GameManager>().gameOver)
+        {
+            transform.Translate(moveDirection.normalized * moveSpeed * Time.deltaTime, Space.World);
+
+            if (transform.position.y < destroyY)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/CloudSpawner.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using System.Collections;
+
+public class CloudSpawner : MonoBehaviour
+{
+    public GameObject[] cloudPrefabs;
+
+    public float minSpawnInterval = 1.0f, maxSpawnInterval = 3.0f;
+    public float spawnOffset = 2.0f, destroyOffset = 2.0f;
+    public float cloudSpeed = 2.0f;
+
+    public Vector2 moveDirection = new Vector2(-0.25f, -1.0f);
+
+    private float spawnY, destroyY, minX, maxX;
+
+    private void Start()
+    {
+        // Calculate the Camera bounds and set the spawnpoint and destroy point of the Clouds used below
+        float worldHeight = Camera.main.orthographicSize * 2.0f;
+        float worldWidth = worldHeight * Camera.main.aspect;
+
+        spawnY = Camera.main.transform.position.y + worldHeight / 2.0f + spawnOffset;
+        destroyY = Camera.main.transform.position.y - worldHeight / 2.0f - destroyOffset;
+
+        minX = Camera.main.transform.position.x - worldWidth / 2.0f;
+        maxX = Camera.main.transform.position.x + worldWidth / 2.0f;
+
+        StartCoroutine(SpawnCloudRoutine());
+    }
+
+    // Subroutine that handles Cloud spawning based on delays set
+    private IEnumerator SpawnCloudRoutine()
+    {
+        while (!FindFirstObjectByType<GameManager>().gameOver)
+        {
+            SpawnCloud();
+            float delay = Random.Range(minSpawnInterval, maxSpawnInterval);
+            yield return new WaitForSeconds(delay);
+        }
+    }
+
+    // Spawn the Clouds
+    private void SpawnCloud()
+    {
+        // Spawn Clouds randomly, no need for a weight system here
+        GameObject prefab = cloudPrefabs[Random.Range(0, cloudPrefabs.Length)];
+
+        Vector3 spawnPosition = new Vector3(Random.Range(minX, maxX), spawnY, 0.0f);
+        GameObject cloudObject = Instantiate(prefab, spawnPosition, Quaternion.identity);
+
+        Cloud cloud = cloudObject.GetComponent<Cloud>();
+
+        // referenced in Cloud script
+        cloud.moveDirection = moveDirection;
+        cloud.moveSpeed = cloudSpeed;
+        cloud.destroyY = destroyY;
+    }
+}
